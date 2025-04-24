44d82f76b2cab60ad2ba204b6b30142b879a45e2
jckstllrd <jackstallard17@gmail.com>
2025-04-24 07:56:29 +0100

    Complete exercises 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let reversedList = [];
+  let stringList = string.split("");
+  for (let i = stringList.length - 1; i >= 0; i--) {
+    reversedList.push(stringList[i]);
+  }
+  let reversedString = reversedList.join("");
+  return reversedString;
 };
 
 // Do not edit below this line
