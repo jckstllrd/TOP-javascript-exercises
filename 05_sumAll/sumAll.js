fad0fed2d9a11b608d77446e505d4bc1f0a87894
jckstllrd <jackstallard17@gmail.com>
2025-04-25 19:21:44 +0100

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let total = 0;
+  let largeNum = num2;
+  let smallNum = num1;
 
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+
+  if (num1 > num2) {
+    largeNum = num1;
+    smallNum = num2;
+  }
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR";
+  }
+  for (let i = smallNum; i <= largeNum; i++) {
+    total += i;
+  }
+  console.log(total);
+
+  return total;
 };
 
 // Do not edit below this line
