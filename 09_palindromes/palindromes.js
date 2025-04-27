5d1fd46a09b824f91766efe990e1949603cafd7d
jckstllrd <jackstallard17@gmail.com>
2025-04-27 20:12:52 +0100

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let punctuation = /[\.,?!]/g;
+  string = string.replace(punctuation, "").toLowerCase().split(" ").join("");
 
+  let backwards = string.split("").reverse().join("").replace(punctuation, "");
+  console.log(backwards);
+
+  if (string == backwards) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
